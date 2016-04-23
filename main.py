d2fd250cda0fc7df87c1ded215e188841849cd60
mamamot <mamamot@gmail.com>
2016-04-23 15:39:54 +0300

    saving last update id

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import telegram
 import json
 
 if __name__ == "__main__":
+    print("Starting the bot...")
     with open("users.json") as f:
+        print("Loading users...")
         user_data = json.load(f)
     with open("last_date.txt") as f:
+        print("Loading time data...")
         last_date = int(f.read().strip())
+    print("Go!")
     telegram.start_bot(user_data, last_date)
--- telegram.py
+++ telegram.py
@@ -1,19 +1,37 @@
 from twx.botapi import TelegramBot
 import time
-
+import json
 
 def return_help():
     pass
 
 
 API_TOKEN = "211900707:AAEHh24_XSCDFWIBonvEMW073H51yKjafFE"
+
+
+
+def return_help(uid, args):
+    bot.send_message(uid, "Здесь будет помощь")
+
+
+def webinar_list(uid, args):
+    bot.send_message(uid, "Здесь будет список семинаров")
+
+
+def register(uid, args):
+    bot.send_message(uid, "Тут будет регистрация на вебинары")
+
 COMMANDS = {
     "/register": register,
-    "/help": return_help
+    "/help": return_help,
+    "/list": webinar_list
 }
 
-
 bot = TelegramBot(API_TOKEN)
+
+
+def greet(uid):
+    bot.send_message(uid, "Привет!")
 
 
 def get_command(message):
@@ -21,6 +39,11 @@ def get_command(message):
     if message[0].strip().startswith("/"):
         return message[0], message[1:]
     return None, None
+
+
+def parse_command(uid, command, args):
+    if command in COMMANDS:
+        COMMANDS[command](uid, args)
 
 
 def start_bot(user_data, last=0):
@@ -32,18 +55,25 @@ def start_bot(user_data, last=0):
                 last_date = update.message.date
                 uid = update.message.sender.id
                 first_name = update.message.sender.first_name
-                if first_name:
-                    bot.send_message(uid, "Привет, дорогой {}".format(first_name))
+                if str(uid) in user_data:
+                    if "last_session" in user_data[uid]:
+                        if time.time() - user_data[uid]["last_session"] > 10800:
+                            greet(uid)
                 else:
-                    bot.send_message(uid, "Привет, дорогой друг!")
+                    user_data[str(uid)] = dict()
+                    greet(uid)
+                user_data[uid]["last_session"] = update.message.date
                 message_text = update.message.text
                 command, command_args = get_command(message_text)
                 print(command)
                 if command:
-                    bot.send_message(uid, "Вау, это же КОМАНДА")
+                    parse_command(uid, command, command_args)
                 print(update)
                 print(update.message.text)
                 bot.send_message(uid, "Отсос")
                 with open("last_date.txt", "w") as w:
                     w.write(str(last_date))
-        time.sleep(5)
+                with open("users.json", "w") as f:
+                    print(user_data)
+                    json.dump(user_data, f)
+        time.sleep(3)
